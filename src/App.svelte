13c8c360dfeb95e8c1ce573d328c9d95781b1a18
codingpalette <codingpalette@gmail.com>
2020-05-19 22:39:44 +0900

    기본 설치

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Router from "svelte-spa-router";
+  import routes from "./routes";
+  import Header from "./components/Header.svelte";
+</script>
+
+<Header />
+
+<Router {routes} />
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+// src/routes.js
+
+import Home from './routes/Home.svelte';
+import About from './routes/About.svelte';
+
+const routes = {
+  '/': Home,
+  '/about': About,
+};
+
+export default routes;

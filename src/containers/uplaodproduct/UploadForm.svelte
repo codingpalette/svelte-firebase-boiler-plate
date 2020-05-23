b246646d7ea30348beefade9e61a796c2c2bda39
codingpalette <codingpalette@gmail.com>
2020-05-23 15:11:36 +0900

    products db 저장 작업

--- src/containers/uplaodproduct/UploadForm.svelte
+++ src/containers/uplaodproduct/UploadForm.svelte
@@ -7,13 +7,29 @@
     { id: 3, text: `야채` },
     { id: 3, text: `과일` }
   ];
+  let formData = {
+    productImages: [],
+    title: "",
+    description: "",
+    selected: questions[0].text,
+    price: ""
+  };
 
-  let selected;
+  const handleSubmit = async () => {
+    console.log(formData);
+    try {
+      await firebase
+        .firestore()
+        .collection("products")
+        .doc(formData.title)
+        .set(formData);
+    } catch (e) {
+      console.log(e);
+    }
 
-  let productImages = [];
-
-  const onClickProductSave = () => {
-    console.log(productImages);
+    // const formData = new FormData(); // Currently empty
+    // console.log(formData);
+    console.log("bbbb");
   };
 </script>
 
@@ -29,14 +45,14 @@
     <h2 class="text-4xl">상품 업로드</h2>
   </div>
 
-  <form>
+  <form on:submit|preventDefault={handleSubmit}>
 
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full px-3">
         <FileUpload
           section="product"
           maxitem="5"
-          bind:imageLists={productImages} />
+          bind:imageLists={formData.productImages} />
       </div>
     </div>
 
@@ -54,7 +70,8 @@
           focus:outline-none focus:bg-white focus:border-purple-500"
           id="name"
           type="text"
-          placeholder="" />
+          placeholder=""
+          bind:value={formData.title} />
       </div>
     </div>
     <div class="flex flex-wrap -mx-3 mb-6">
@@ -71,7 +88,8 @@
           focus:outline-none focus:bg-white focus:border-purple-500 "
           id="description"
           type="text"
-          placeholder="" />
+          placeholder=""
+          bind:value={formData.description} />
       </div>
     </div>
 
@@ -86,7 +104,7 @@
         <select
           name="category"
           id="category"
-          bind:value={selected}
+          bind:value={formData.selected}
           class="block appearance-none w-full bg-gray-200 border-2
           border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight
           focus:outline-none focus:bg-white focus:border-purple-500">
@@ -110,13 +128,13 @@
           focus:outline-none focus:bg-white focus:border-purple-500"
           id="price"
           type="number"
-          placeholder="" />
+          placeholder=""
+          bind:value={formData.price} />
       </div>
     </div>
     <button
-      type="button"
-      class="bg-purple-500 hover:bg-purple-400 rounded px-4 py-2 text-white"
-      on:click={onClickProductSave}>
+      type="submit"
+      class="bg-purple-500 hover:bg-purple-400 rounded px-4 py-2 text-white">
       업로드
     </button>
 
--- src/routes/UplaodProduct.svelte
+++ src/routes/UplaodProduct.svelte
@@ -1,5 +1,14 @@
 <script>
+  import { userLevel } from "../store/UserStore";
+  import { onMount } from "svelte";
+  import { push } from "svelte-spa-router";
   import UploadForm from "../containers/uplaodproduct/UploadForm.svelte";
+
+  onMount(() => {
+    if ($userLevel !== 0) {
+      push("/");
+    }
+  });
 </script>
 
 <style>

eba3665ba713e3a86dab6d810f04e31bcecb5675
codingpalette <codingpalette@gmail.com>
2020-05-30 00:31:08 +0900

    cart 컴포넌트 수정

--- src/containers/keepProducts/Cart.svelte
+++ src/containers/keepProducts/Cart.svelte
@@ -47,47 +47,81 @@
   .header_text_box {
     border-bottom: 1px solid #d2d2d7;
   }
-  .item_list {
-    /* border-bottom: 1px solid #d2d2d7; */
+  .image_box {
+    width: 120px;
+    height: 120px;
+    position: relative;
+    overflow: hidden;
+    background-color: #fff;
   }
-  .item_img {
-    max-width: 200px;
+  .image_box img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
-  .item_text_box {
-    margin-left: 1rem;
-    flex: 1;
+  .list_box {
+    background-color: #fafafa;
   }
-  .list {
-    line-height: 1.2;
-    margin-bottom: 1rem;
-    padding-bottom: 1rem;
-    border-bottom: 1px solid #d2d2d7;
+  .item_list .card_box {
+    width: 100%;
   }
-  .list:last-child {
-    margin-bottom: 0;
-    padding-bottom: 0;
-    border: none;
+  .item_list .result_box {
+    width: 100%;
+  }
+
+  @media screen and (min-width: 768px) {
+    .item_list .card_box {
+      width: 47.5%;
+    }
+    .item_list .result_box {
+      width: 47.5%;
+    }
   }
 </style>
 
-<section class="cart_container">
+<section class="cart_container px-4 box-border">
   <div class="max-w-screen-lg pt-24 mr-auto ml-auto">
-    <h2 class=" text-center text-3xl font-bold">
-      장바구니에 들어 있는 제품입니다.
-    </h2>
+    <h2 class=" text-center text-3xl font-bold">장바구니</h2>
     <div class="pt-4 pb-12 header_text_box">
       <p class="text-sm text-center text-gray-600 leading-normal">
         모든 주문에 무료 배송 서비스가 제공됩니다.
       </p>
     </div>
     {#if cartOk}
-      <div class="mt-12 pb-12 item_list">
-        <ol>
+      <div class="mt-12 pb-12 item_list flex justify-between flex-wrap">
+        <ol class="card_box">
           {#each cartItems as item}
-            <li
-              class="shadow-lg rounded border-solid border border-gray-100
-              overflow-hidden box-border mb-8">
-              <div class="flex">
+            <li class="flex items-center mb-8 ">
+              <div
+                class="list_box border-solid border border-gray-300
+                overflow-hidden box-border p-4 flex-1 flex items-center
+                flex-wrap">
+                <div class="image_box rounded-full">
+                  <img src={item.image} alt="" />
+                </div>
+                <div class="name_box text-xl mx-4">
+                  <a
+                    href="/product/{item.id}"
+                    use:link
+                    class="text-purple-500 hover:underline">
+                    {item.title}
+                  </a>
+                </div>
+                <div class="count_box">
+                  <span>{item.quantity}개</span>
+                </div>
+                <div class="price_box ml-auto">
+                  <span>₩{item.price.toLocaleString()}</span>
+                </div>
+              </div>
+              <button
+                class="ml-4 bg-gray-200 hover:bg-gray-300 text-black h-10 w-10
+                flex items-center justify-center rounded-full">
+                <i class="fas fa-times" />
+              </button>
+
+              <!-- <div class="flex">
                 <div class="item_img">
                   <img src={item.image} alt="" />
                 </div>
@@ -110,10 +144,11 @@
                     <span>{item.quantity}</span>
                   </div>
                 </div>
-              </div>
+              </div> -->
             </li>
           {/each}
         </ol>
+        <div class="result_box">asdasd</div>
       </div>
     {/if}
 

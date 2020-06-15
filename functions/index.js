3c8e0d730173dad8501d89bb54ac555da9ea0bf0
codingpalette <codingpalette@gmail.com>
2020-06-15 13:02:16 +0900

    fufirebase nction 수정

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-var serviceAccount = require('./key.json');
+const serviceAccount = require('./key.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: functions.config().admin.db_url, //'https://svelte-firebase-boiler-plate.firebaseio.com',
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: functions.config().admin.db_url, //'https://svelte-firebase-boiler-plate.firebaseio.com',
 });
 
 // admin.initializeApp();
@@ -15,31 +15,36 @@ const realdb = admin.database();
 // console.log(functions.config().admin.email);
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+    response.send('Hello from Firebase!');
 });
 
 exports.createUser = functions.auth.user().onCreate(async (user) => {
-  const { uid, email, displayName, emailVerified, photoURL, disabled } = user;
-  const claims = { level: 2 };
-  if (functions.config().admin.email === user.email && user.emailVerified) claims.level = 0;
-  await admin.auth().setCustomUserClaims(uid, claims);
-  const d = {
-    uid,
-    email,
-    displayName,
-    emailVerified,
-    photoURL,
-    disabled,
-    createdAt: new Date().getMilliseconds(),
-    level: claims.level,
-  };
+    const {uid, email, displayName, emailVerified, photoURL, disabled} = user;
+    const claims = {level: 2};
+    if (functions.config().admin.email === user.email && user.emailVerified) claims.level = 0;
+    await admin.auth().setCustomUserClaims(uid, claims);
+    const d = {
+        uid,
+        email,
+        displayName,
+        emailVerified,
+        photoURL,
+        disabled,
+        createdAt: new Date().getMilliseconds(),
+        level: claims.level,
+        address : {
+            road : '',
+            road_detail : '',
+            postcode : '',
+        }
+    };
 
-  const r = await db.collection('users').doc(uid).set(d);
-  realdb.ref('users').child(uid).set(d);
-  return r;
+    const r = await db.collection('users').doc(uid).set(d);
+    // realdb.ref('users').child(uid).set(d);
+    return r;
 });
 
 exports.deleteUser = functions.auth.user().onDelete((user) => {
-  db.collection('users').doc(user.uid).delete();
-  realdb.ref('users').child(user.uid).remove();
+    db.collection('users').doc(user.uid).delete();
+    // realdb.ref('users').child(user.uid).remove();
 });
